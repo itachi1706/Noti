0c6ed857d8c3d8047b2328f5b45e04b9edba6ecf
Kenneth Soh <kennethsohyq@gmail.com>
2020-10-20 23:18:50 +0800

    Updated main project to Swift 5 Context

--- Noti/IntroViewController.swift
+++ Noti/IntroViewController.swift
@@ -55,9 +55,9 @@ class IntroViewController: NSViewController {
     }
     
     @IBAction func startAuth(_ sender: AnyObject) {
-        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
         
-        awc = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "AuthWindowController")) as? NSWindowController
+        awc = storyboard.instantiateController(withIdentifier: "AuthWindowController") as? NSWindowController
         print("showWindow")
         awc!.showWindow(self)
     }
